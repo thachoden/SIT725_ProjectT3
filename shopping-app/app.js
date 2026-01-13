1b95e1160f02ae7fe040efe60dbf44cf79588dab
KexinChengWednesday <3297538022@qq.com>
2026-01-13 16:01:27 +1100

    chore: add path module for static file support

--- shopping-app/app.js
+++ shopping-app/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,8 +9,15 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-
+// body parser
+app.use(express.urlencoded({ extended: true }));
 // Your routes and middlewares here
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const router = require('./routes/router');
+app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
